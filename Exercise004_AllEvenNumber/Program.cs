dfd47967c462e972102b850981422d21525731b3
tatiana_sht <tatianaorlova0271@gmail.com>
2023-06-13 15:43:15 +1000

    Added solution

---
+++ Exercise004_AllEvenNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+
+Console.Write($"В числе {number} четными являются числа - ");
+
+while (result <= number)
+{
+    if (result % 2 == 0)
+        Console.Write(result + " ");
+    result++;
+}
